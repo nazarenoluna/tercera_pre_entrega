0d5248e44027e2536f1303e973fce62f3f083d1d
Nazareno Luna <nazarenogluna12@hotmail.com>
2023-01-16 18:50:58 -0300

    Creando views, urls, models,templates

--- AppCoder/views.py
+++ AppCoder/views.py
@@ -1,3 +1,85 @@
-from django.shortcuts import render
+from datetime import date
 
-# Create your views here.
+from django.db.models import Q
+from django.shortcuts import render, redirect
+from django.urls import reverse
+from django.http import HttpResponse
+
+from AppCoder.models import Estudiante, Profesor, Curso
+from AppCoder.forms import CursoFormulario
+
+# Creo mis vistas aqui
+def inicio(request):
+    return render(
+        request=request,
+        template_name='AppCoder/inicio.html',
+    )
+
+
+def listar_estudiantes(request):
+    ## Aqui iria la validacion del permiso lectura estudiantes
+    contexto = {
+        'estudiantes': Estudiante.objects.all()
+    }
+    return render(
+        request=request,
+        template_name='AppCoder/lista_estudiantes.html',
+        context=contexto,
+    )
+
+
+def listar_profesores(request):
+    contexto = {
+        'profesores': Profesor.objects.all()
+    }
+    return render(
+        request=request,
+        template_name='AppCoder/lista_profesores.html',
+        context=contexto,
+    )
+
+
+def listar_cursos(request):
+    contexto = {
+        'cursos': Curso.objects.all()
+    }
+    return render(
+        request=request,
+        template_name='AppCoder/lista_cursos.html',
+        context=contexto,
+    )
+
+
+def crear_curso(request):
+    if request.method == "POST":
+        formulario = CursoFormulario(request.post)
+
+        if formulario.is_valid():
+            data = formulario.cleaned_data
+            curso = Curso(nombre=data['nombre'],comision=data['comision'])
+            curso.save()
+            url_exitosa = reverse('listar_cursos')
+            return redirect(url_exitosa)
+        
+    else:  # GET
+        formulario = CursoFormulario()
+    return render(
+        request=request,
+        template_name='AppCoder/formulario_curso.html',
+        context={'formulario': formulario},
+    ) 
+
+
+def buscar_cursos(request):
+    if request.method == "POST":
+        data = request.POST
+        cursos = Curso.objects.filter(
+            Q(nombre__contains=data['busqueda']) | Q(comision__exact=data['busqueda'])
+        )
+        contexto ={
+            'cursos': cursos
+        }
+        return render(
+            request=request,
+            template_name='AppCoder/lista_cursos.html',
+        )
